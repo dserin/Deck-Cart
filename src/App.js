11998f95deace69b03abb9111d27a41b686847dd
Doğacan Serin <dogacanserin@gmail.com>
2022-10-11 23:54:13 +0300

    Deck-Cart-Play

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CardGame from "./components/CardGame";
+
+function App() {
+  return (
+    <div className="App">
+      <CardGame />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardGame.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import "./CardGame.css";
+import Card from "./Card";
+
+export class CardGame extends Component {
+  state = {
+    deckId: "",
+    cardsRemaining: "52",
+    cards: [],
+  };
+
+  async componentDidMount() {
+    const deck = await axios.get(
+      "https://deckofcardsapi.com/api/deck/new/shuffle/"
+    );
+    this.setState({ deckId: deck.data.deck_id });
+  }
+
+  getCard = async () => {
+    const response = await axios.get(
+      `https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/`
+    );
+
+    const rotate = Math.floor(Math.random() * 81 - 40);
+    const transY = Math.floor(Math.random() * 41 - 20);
+    const transX = Math.floor(Math.random() * 41 - 20);
+    const transformStyle = {
+      transform: `translateX(calc(-50% + ${transX}px)) 
+            translateY(${transY}px) rotate(${rotate}deg)`,
+    };
+
+    const newCard = { ...response.data.cards[0], style: transformStyle };
+    this.setState((prevSt) => ({
+      cardsRemaining: response.data.remaining,
+      cards: [...prevSt.cards, newCard],
+    }));
+  };
+
+  render() {
+    const { cards, cardsRemaining } = this.state;
+    return (
+      <div className="CardGame">
+        <button onClick={this.getCard} disabled={cardsRemaining === 0}>
+          Gimme a Card
+        </button>
+
+        <p>Cards Remaining: {cardsRemaining}</p>
+        <div className="CardGame__cards">
+          {cards.map((card) => (
+            <Card {...card} key={card.code} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CardGame;
